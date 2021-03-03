6c72407dffffcc019d708115b2cbae5324f13721
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-03-03 12:34:39 +0200

    Add incorrect-test

--- DSL/DSL/Program.cs
+++ DSL/DSL/Program.cs
@@ -8,24 +8,46 @@ namespace DSL
     {
         static void Main(string[] args)
         {
+            string correctFilePath = "./../../../../../correct-test.txt";
+            string incorrectFilePath = "./../../../../../incorrect-test.txt";
 
-            StreamReader sr = new StreamReader("./../../../../../correct-test.txt");
+
+            Console.WriteLine("------------------Analyze correct program code------------------");
+            Analyze(correctFilePath);
+
+
+            Console.WriteLine("------------------Analyze incorrect program code------------------");
+            Analyze(incorrectFilePath);
+
+
+        }
+
+        private static void Analyze(string filePath)
+        {
+
+            StreamReader sr = new StreamReader(filePath);
             string text = sr.ReadToEnd();
+
+            Console.WriteLine(text);
+            Console.WriteLine();
 
             LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(text);
             if (lexicalAnalyzer.Analyze())
             {
-                Console.WriteLine(text);
-                Console.WriteLine();
+               
+                Console.ForegroundColor = ConsoleColor.Green;
                 showTokens(lexicalAnalyzer.Tokens);
+                Console.ResetColor();
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(lexicalAnalyzer.ErrorMsg);
+                Console.ResetColor();
             }
-           
-        }
 
+            Console.WriteLine();
+        }
 
         static void showTokens(Queue<Token> tokens)
         {
