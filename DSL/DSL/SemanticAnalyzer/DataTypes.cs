bec84816a2fe263999f0782c62f053df9dd67d3d
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-03-30 00:19:18 +0300

    feat: add semantic analyzer

---
+++ DSL/DSL/SemanticAnalyzer/DataTypes.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSL
+{
+    public class DataTypes
+    {
+        public const string PRIVATE_ENTERPRENUER_DT = "private-entrepreneur";
+        public const string FULL_NAME_DT = "full-name";
+        public const string ADDRESS_DT = "address";
+        public const string PHONE_DT = "phone";
+        public const string RNTRC_DT = "RNTRC";
+        public const string GROUP_DT = "group";
+        public const string TAX_SYSTEM_DT = "tax-system";
+        public const string NACE_CODE_DT = "NACE-code";
+        public const string TYPE_ONE_TAX_PAYER_DT = "type-one-tax-payer";
+        public const string SFS_CODE_DT = "SFS-code";
+        public const string YEAR_DT = "year";
+        public const string QUARTER_DT = "quarter";
+        public const string MONTH_DT = "month";
+        public const string INCOME_DT = "income";
+        public const string SPECIFIED_AMOUNT_DT = "specified-amount";
+        public const string SUBMISSION_DATE_DT = "submission-date";
+        public const string TYPE_UNIFIED_SOCIAL_TAX_DT = "type-unified-social-tax";
+        public const string START_DATE_DT = "start-date";
+        public const string LAST_DATE_DT = "LAST-date";
+        public const string TABLE_1_UNIFIED_SOCIL_TAX_DT = "table-1-unified-social-tax";
+        public const string INCOME_DECLARED_DT = "income-declared";
+        public const string INCOME_AMOUNT_DT = "income-amount";
+        public const string TYPE_1_DF_DT = "type-1DF";
+        public const string EMPLOYEE_BY_CIVIL_CONRACT_DT = "employee-by-civil-contract";
+        public const string EMPLOYEE_BY_EMPLOYMENT_CONRACT_DT = "employee-by-employment-contract";
+        public const string SECTION_1_DF_1_DT = "section-1-DF1";
+        public const string EMPLOYEE_ITN_DT = "employee-ITN";
+        public const string INCOME_ACCURED_DT = "income-accured";
+        public const string INCOME_PAID_DT = "income-paid";
+        public const string INCOME_SIGN_DT = "income-sign";
+        public const string DATE_ADOPT_DT = "date-adopt";
+        public const string DATE_DISMISS_DT = "date-dismiss";
+        public const string SIGN_DT = "sign";
+        public const string UNIFIED_SOCIAL_TAX_REPORT_DT = "unified-social-tax-report";
+        public const string ONE_TAX_PAYER_REPORT_DT = "one-tax-payer-report";
+        public const string DF_1_REPORT_DT = "DF1-report";
+
+        public const string NEW_METHOD = "new";
+        public const string ADD_NEW_NACE_CODE_METHOD = "add-new-NACE-code";
+        public const string SET_MAIN_NACE_CODE_METHOD = "set-main-NACE-code";
+
+        public const string CHANGE_SINGLE_TAX_PERCENT_METHOD = "change-single-tax-percent";
+        public const string CLARIFY_METHOD = "clarify";
+        public const string SHOW_METHOD = "show";
+        public const string SAVE_METHOD = "save";
+        public const string ADD_MONTH_DATA_METHOD = "add-month-data";
+        public const string ADD_NEW_RECORD_METHOD = "add-new-record";
+        public const string DELETE_RECORD_METHOD = "delete-record";
+        public const string CLARIFY_ADD_NEW_RECORD_METHOD = "clarify-add-new-record";
+        public const string CLARIFY_DELETE_RECORD_METHOD = "clarify-delete-record";
+        public const string NUMBER_DT = "number";
+
+        public List<string> PropsForPrEnt = new List<string>{FULL_NAME_DT, ADDRESS_DT, PHONE_DT,
+            RNTRC_DT, NACE_CODE_DT, GROUP_DT, TAX_SYSTEM_DT};
+        public List<string> PropsForOneTaxPayerReport = new List<string> { PRIVATE_ENTERPRENUER_DT, TYPE_ONE_TAX_PAYER_DT, SFS_CODE_DT, YEAR_DT,
+            QUARTER_DT, MONTH_DT, INCOME_DT, SPECIFIED_AMOUNT_DT, SUBMISSION_DATE_DT};
+        public List<string> PropsForUnifiedSocialTaxReport = new List<string> { PRIVATE_ENTERPRENUER_DT, TYPE_UNIFIED_SOCIAL_TAX_DT, SFS_CODE_DT, START_DATE_DT,
+            LAST_DATE_DT, SUBMISSION_DATE_DT, TABLE_1_UNIFIED_SOCIL_TAX_DT};
+        public List<string> PropsForT1UnSocTax = new List<string> { MONTH_DT, INCOME_DECLARED_DT, INCOME_AMOUNT_DT };
+        public List<string> PropsFor1DFReport = new List<string> { PRIVATE_ENTERPRENUER_DT, TYPE_1_DF_DT, QUARTER_DT, YEAR_DT, EMPLOYEE_BY_EMPLOYMENT_CONRACT_DT,
+            SECTION_1_DF_1_DT};
+        public List<string> PropsForSection1DF1 = new List<string> { EMPLOYEE_ITN_DT, INCOME_ACCURED_DT, INCOME_PAID_DT, INCOME_SIGN_DT, DATE_ADOPT_DT,
+            DATE_DISMISS_DT, SIGN_DT};
+
+
+        public List<string> MethodsForPrEnt = new List<string> { NEW_METHOD, ADD_NEW_NACE_CODE_METHOD, SET_MAIN_NACE_CODE_METHOD };
+        public List<string> MethodsForOneTaxPayerReport = new List<string> { NEW_METHOD, CHANGE_SINGLE_TAX_PERCENT_METHOD, CLARIFY_METHOD,
+            SHOW_METHOD, SAVE_METHOD};
+        public List<string> MethodsForUnifiedSocialTaxReport = new List<string> { NEW_METHOD, SHOW_METHOD, SAVE_METHOD };
+        public List<string> MethodsForT1UnSocTax = new List<string> { NEW_METHOD, ADD_MONTH_DATA_METHOD };
+        public List<string> MethodsFor1DFReport = new List<string> { NEW_METHOD, SHOW_METHOD, SAVE_METHOD };
+        public List<string> MethodsForSection1DF1 = new List<string> { ADD_NEW_RECORD_METHOD, DELETE_RECORD_METHOD,
+            CLARIFY_ADD_NEW_RECORD_METHOD, CLARIFY_DELETE_RECORD_METHOD};
+
+        public List<string> AttrsForAddNewNaceCode = new List<string> { NACE_CODE_DT };
+        public List<string> AttrsForSetMainNaceCode = new List<string> { NACE_CODE_DT };
+        public List<string> AttrsForChangeSingleTaxPercent = new List<string> { NUMBER_DT };
+        public List<string> AttrsForClarify = new List<string> { SPECIFIED_AMOUNT_DT };
+        public List<string> AttrsForShow = new List<string>();
+        public List<string> AttrsForSave = new List<string>();
+        public List<string> AttrsForAddMonthData = new List<string> { MONTH_DT, INCOME_DECLARED_DT, INCOME_AMOUNT_DT };
+        public List<string> AttrsForAddNewRecord = new List<string> { EMPLOYEE_ITN_DT, INCOME_ACCURED_DT, INCOME_DECLARED_DT, INCOME_SIGN_DT,
+            DATE_ADOPT_DT, DATE_DISMISS_DT};
+        public List<string> AttrsForDeleteRecord = new List<string> { EMPLOYEE_ITN_DT, INCOME_ACCURED_DT, INCOME_DECLARED_DT, INCOME_SIGN_DT,
+            DATE_ADOPT_DT, DATE_DISMISS_DT};
+        public List<string> AttrsForClarifyAddNewRecord = new List<string> {EMPLOYEE_ITN_DT, INCOME_ACCURED_DT, INCOME_DECLARED_DT, INCOME_SIGN_DT,
+            DATE_ADOPT_DT, DATE_DISMISS_DT };
+        public List<string> AttrsForClarifyDeleteRecord = new List<string> {EMPLOYEE_ITN_DT};
+
+    }
+}
---
+++ DSL/DSL/SemanticAnalyzer/SemanticAnalyzer.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSL
+{
+   
+    public class SemanticAnalyzer
+    {
+        public DataTypes DataTypes { get; }
+
+        public Dictionary<string, string> InitializationVariables { get; set; }
+
+        public List<AbstractExpression> Expressions { get; set; }
+
+        public SemanticAnalyzer(List<AbstractExpression> expressions)
+        {
+            Expressions = expressions;
+            DataTypes = new DataTypes();
+            InitializationVariables = new Dictionary<string, string>();
+        }
+
+        public void Analyze()
+        {
+            foreach(var expr in Expressions)
+            {
+                AnalyzeExpression(expr);
+            }
+        }
+
+        private void AnalyzeExpression(AbstractExpression expr)
+        {
+            if (expr is InitializationExpression)
+                AnalyzeInitializationExpr((InitializationExpression)expr);
+            else if (expr is AssignmentExpression)
+                AnalyzeAssignmentExpr((AssignmentExpression)expr);
+            else if (expr is DeclarationExpression)
+                AnalyzeDecExpr((DeclarationExpression)expr);
+            else if (expr is OperationExpression)
+                AnalyzeOpExpr((OperationExpression)expr);
+        }
+
+        private void AnalyzeOpExpr(OperationExpression expr)
+        {
+            if (!InitializationVariables.ContainsKey(expr.identifier))
+                throw new Exception($"{expr.identifier} isn't initialized");
+            if (!checkPropForMethod(expr.methodName, InitializationVariables[expr.identifier]))
+                throw new Exception($"Method ${expr.methodName} isn't available for {InitializationVariables[expr.identifier]} data type");
+            checkMethodAttrs(expr.methodName, expr.attributes);
+        }
+
+        private void checkMethodAttrs(string method, List<string> attributes)
+        {
+            List<string> lst = new List<string>();
+            switch (method)
+            {
+                case DataTypes.ADD_NEW_NACE_CODE_METHOD:
+                    lst = DataTypes.AttrsForAddNewNaceCode;
+                    break;
+                case DataTypes.SET_MAIN_NACE_CODE_METHOD:
+                    lst = DataTypes.AttrsForSetMainNaceCode;
+                    break;
+                case DataTypes.CHANGE_SINGLE_TAX_PERCENT_METHOD:
+                    lst = DataTypes.AttrsForChangeSingleTaxPercent;
+                    break;
+                case DataTypes.CLARIFY_METHOD:
+                    lst = DataTypes.AttrsForClarify;
+                    break;
+                case DataTypes.SHOW_METHOD:
+                    lst = DataTypes.AttrsForShow;
+                    break;
+                case DataTypes.SAVE_METHOD:
+                    lst = DataTypes.AttrsForSave;
+                    break;
+                case DataTypes.ADD_MONTH_DATA_METHOD:
+                    lst = DataTypes.AttrsForAddMonthData;
+                    break;
+                case DataTypes.ADD_NEW_RECORD_METHOD:
+                    lst = DataTypes.AttrsForAddNewRecord;
+                    break;
+                case DataTypes.DELETE_RECORD_METHOD:
+                    lst = DataTypes.AttrsForDeleteRecord;
+                    break;
+                case DataTypes.CLARIFY_ADD_NEW_RECORD_METHOD:
+                    lst = DataTypes.AttrsForClarifyAddNewRecord;
+                    break;
+                case DataTypes.CLARIFY_DELETE_RECORD_METHOD:
+                    lst = DataTypes.AttrsForClarifyDeleteRecord;
+                    break;
+                default: 
+                    break;
+            }
+
+            if (attributes.Count != lst.Count)
+                throw new Exception($"{method} doesn't have appropriate attributes");
+
+            for(int i=0; i<lst.Count; i++)
+            {
+                checkCorrectValueForType(lst[i], attributes[i]);
+            }
+        }
+
+        private bool checkPropForMethod(string method, string DT)
+        {
+            List<string> lst = new List<string>();
+            switch (DT)
+            {
+                case DataTypes.PRIVATE_ENTERPRENUER_DT:
+                    lst = DataTypes.MethodsForPrEnt;
+                    break;
+                case DataTypes.ONE_TAX_PAYER_REPORT_DT:
+                    lst = DataTypes.MethodsForOneTaxPayerReport;
+                    break;
+                case DataTypes.UNIFIED_SOCIAL_TAX_REPORT_DT:
+                    lst = DataTypes.MethodsForUnifiedSocialTaxReport;
+                    break;
+                case DataTypes.TABLE_1_UNIFIED_SOCIL_TAX_DT:
+                    lst = DataTypes.MethodsForT1UnSocTax;
+                    break;
+                case DataTypes.DF_1_REPORT_DT:
+                    lst = DataTypes.MethodsFor1DFReport;
+                    break;
+                case DataTypes.SECTION_1_DF_1_DT:
+                    lst = DataTypes.MethodsForSection1DF1;
+                    break;
+                default:
+                    break;
+            }
+            return isLstContain(method, lst);
+
+        }
+
+        private void AnalyzeDecExpr(DeclarationExpression expr)
+        {
+            AnalyzeInitializationExpr(expr.initializationExpression);
+            AnalyzeExpr(expr.expression, expr.initializationExpression.keyWord);
+        }
+
+        private void AnalyzeAssignmentExpr(AssignmentExpression expr)
+        {
+            string leftDT;
+            if (expr.identifier != null)
+            {
+                if (!IsIdInitialized(expr.identifier))
+                    createExNotInitializedExpr(expr.identifier);
+                leftDT = InitializationVariables[expr.identifier];
+            }
+            else if (expr.propertiesExpression != null)
+            {
+                AnalyzePropExpr(expr.propertiesExpression);
+                leftDT = expr.propertiesExpression.keyWord;
+            }
+            else
+                throw new Exception("Can not read left part of assignment expression");
+
+            if (expr.Right != null)
+                AnalyzeExpr(expr.Right, leftDT);
+            else
+                throw new Exception("Can not read right part of assignment expression");
+        }
+
+        private void AnalyzePropExpr(PropertyExpression expr)
+        {
+            if (!IsIdInitialized(expr.identifier))
+                createExNotInitializedExpr(expr.identifier);
+            if(!checkPropForId(expr.keyWord, InitializationVariables[expr.identifier]))
+                throw new Exception($"For {expr.identifier} property {expr.keyWord} isn't use");
+        }
+
+        private bool checkPropForId(string keyWord, string DT)
+        {
+            List<string> lst = new List<string>();
+            switch (DT)
+            {
+                case DataTypes.PRIVATE_ENTERPRENUER_DT:
+                    lst = DataTypes.PropsForPrEnt;
+                    break;
+                case DataTypes.ONE_TAX_PAYER_REPORT_DT:
+                    lst = DataTypes.PropsForOneTaxPayerReport;
+                    break;
+                case DataTypes.UNIFIED_SOCIAL_TAX_REPORT_DT:
+                    lst = DataTypes.PropsForUnifiedSocialTaxReport;
+                    break;
+                case DataTypes.TABLE_1_UNIFIED_SOCIL_TAX_DT:
+                    lst = DataTypes.PropsForT1UnSocTax;
+                    break;
+                case DataTypes.DF_1_REPORT_DT:
+                    lst = DataTypes.PropsFor1DFReport;
+                    break;
+                case DataTypes.SECTION_1_DF_1_DT:
+                    lst = DataTypes.PropsForSection1DF1;
+                    break;
+                default:
+                    break;
+            }
+            return isLstContain(keyWord, lst);                
+        }
+
+        private bool isLstContain(string keyWord, List<string> propsLst)
+        {
+            bool a = propsLst.Contains(keyWord);
+            return a;
+        }
+
+        private void createExNotInitializedExpr(string identifier)
+        {
+            throw new Exception($"Can't find initialized identifier {identifier}");
+        }
+
+        private void AnalyzeExpr(Expression expr, string leftDT)
+        {
+            if(expr.keyWord != null && expr.definitionExpressions!= null)
+            {
+                if (leftDT != expr.keyWord)
+                    throw new Exception("Right part of expression doesn't match type in left part");
+                AnalyzeDefExpr(expr.definitionExpressions, leftDT);
+            }else if(expr.Identifier != null)
+            {
+                checkCorrectValueForType(leftDT, expr.Identifier);
+            }
+        }
+
+        private void AnalyzeDefExpr(List<DefinitionExpression> exprs, string leftDT)
+        {
+            foreach(var expr in exprs)
+            {
+                if (!checkPropForId(expr.keyWord, leftDT))
+                    throw new Exception($"Property {expr.keyWord} isn't use for {leftDT}");
+                checkCorrectValueForType(expr.keyWord, expr.identifier);
+            }
+        }
+
+        private void AnalyzeInitializationExpr(InitializationExpression expr)
+        {
+            if (IsIdInitialized(expr.identifier))
+                throw new Exception("You try to initialize identifier again");
+            AddNewID(expr.identifier, expr.keyWord);
+        }
+
+        private void AddNewID(string identifier, string keyWord)
+        {
+            InitializationVariables.Add(identifier, keyWord);
+        }
+
+        private bool IsIdInitialized(string identifier)
+        {
+            return InitializationVariables.ContainsKey(identifier);
+        }
+
+        private void checkCorrectValueForType(string dataType, string value)
+        {
+            int a = 10;
+        }
+    }
+}
